21d5fb535688e302abb271b6902cb8d270507fb2
姜旗 <1292692959@qq.com>
2021-06-22 10:51:15 +0800

    chore: dist

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   title: 'SoonSpacejs Blog',
+  base: '/',
+  dest: 'dist',
   theme: 'simple',
   themeConfig: {
     author: 'SoonSpacejs Team', // will display on the page footer
