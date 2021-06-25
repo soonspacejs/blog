ed3554f2e58172c386579ded02b6c84dabe68265
xuekai-china <ee_shadow@163.com>
2021-06-25 17:37:25 +0800

    feat: theme style

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'SoonSpacejs Blog',
+  base: '/blog/',
+  dest: 'dist',
+  theme: 'reco',
+  themeConfig: {
+    author: 'SoonSpacejs Team',
+    type: 'blog'
+  },
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+---
+home: true
+title: SoonSpacejs Blog
+description: ' '
+bgImage: ./bg.png
+bgImageStyle: {
+  height: '400px',
+  padding: '24px',
+  align-items: flex-end
+}
+---
